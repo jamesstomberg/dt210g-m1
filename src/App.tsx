7e9beb1ca8cfbb90301cc773a8c61cfaa9341384
James Stomberg <james@vistrom.se>
2025-01-19 21:50:55 +0100

    Import new components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 import './App.css';
 
 function App() {
     return (
         <>
             <Header title="Dynamiska hundar" />
+            <Main title="Samlingen hämtas via Dog API" />
+            <Footer />
         </>
     );
 }
