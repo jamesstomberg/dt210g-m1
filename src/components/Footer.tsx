d56d4f456ef6254ffd69f982a42c1f574b79c1cc
James Stomberg <james@vistrom.se>
2025-01-19 21:51:25 +0100

    Add footer component including styles.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import '../assets/Footer.scss';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer__inner">
+                <span>DT210G</span>
+                <span>Mittuniversitetet</span>
+                <span>James Stomberg</span>
+            </div>
+        </footer>
+    );
+}
